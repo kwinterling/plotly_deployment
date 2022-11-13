b9ccdc935b220dbb0f3a5041fe085b8e32aabb8b
Karl Winterling <kwinterling@gmail.com>
2022-11-13 10:37:55 -0800

    Added Bootstrap and CSS to make the page prettier and better organized

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,1 +1,10 @@
 Plotly.newPlot("plotArea", [{x: [1, 2, 3], y: [10, 20, 30]}]);
+
+Plotly.newPlot("myPlot", [{x: [1, 2, 3, 4, 5], y: [2, 4, 6, 8, 10]}]);
+
+var trace = {
+    x: ["burrito", "pizza", "chicken"],
+    y: [10, 18, 5],
+    type: "bar"
+ };
+ Plotly.newPlot("plotAreaBar1", [trace]);
